df482ae4f5c9a3a61e65e11a1241ba94bff47010
Naykeu Araújo Luz <162498773+BlackRobinn@users.noreply.github.com>
2025-05-25 18:28:30 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,18 @@
 // firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEHvY7F90VDJAxGYpU0cjqVizNfZmejRA",
   authDomain: "autenticacao-administrativa.firebaseapp.com",
+  databaseURL: "https://autenticacao-administrativa-default-rtdb.firebaseio.com",
   projectId: "autenticacao-administrativa",
-  storageBucket: "autenticacao-administrativa.firebasestorage.app",
+  storageBucket: "autenticacao-administrativa.appspot.com",
   messagingSenderId: "724706435418",
   appId: "1:724706435418:web:cc90055d710060cf381363"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { database };
